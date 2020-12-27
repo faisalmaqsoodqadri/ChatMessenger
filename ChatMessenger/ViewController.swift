3bf8bded78176fa97e5062310f8c55ebeebb0d09
Faisal Maqsood <fmqswift@yahoo.com>
2020-12-27 19:52:47 +0200

    First Chat Project by Faisal Maqsood Qadri

---
+++ ChatMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatMessenger
+//
+//  Created by Faisal Maqsood Qadri on 27/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
